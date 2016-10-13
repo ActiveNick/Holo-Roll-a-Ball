7c1500eef4e406be1ac801e13fbd051930595a49
Nick Landry <nlandry@activenick.net>
2016-10-12 22:02:30 -0400

    Added Xbox One S controller support via plugin

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+// Required for the Hololens Controller Input plugin for Hololens
+// I'm using this temprorarily until Unity the Unity input manager 
+// supports Bluetooth controllers on HoloLens like the Xbox One S Controller
+using HoloLensXboxController;
 
 public class PlayerController : MonoBehaviour {
 
@@ -10,26 +14,48 @@ public class PlayerController : MonoBehaviour {
     public Text countText;
     public Text winText;
 
-	// Initialization
-	void Start () {
+    // Temporarily used by the HoloLensXboxController plugin
+    private ControllerInput controllerInput;
+
+    // Initialization
+    void Start () {
         rb = GetComponent<Rigidbody>();
         ResetUI();
+
+        // HoloLensXboxController plugin initialization
+        // First parameter is the number, starting at zero, of the controller you want to follow.
+        // Second parameter is the default “dead” value; meaning all stick readings less than this value will be set to 0.0.
+        controllerInput = new ControllerInput(0, 0.19f);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 
     // FixedUpdate is called just before performing any physics calculations
     void FixedUpdate ()
     {
-        // This code lets us use either keyboard arrow or WASD keys, as well
-        // as a controller (left thumbstick) which is ideal for VR/MR
-        float moveHortizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal;
+        float moveVertical;
 
-        Vector3 movement = new Vector3(moveHortizontal, 0, moveVertical);
+#if (UNITY_EDITOR)
+        {
+            // This code lets us use either keyboard arrow or WASD keys, as well
+            // as a controller (left thumbstick) which is ideal for VR/MR
+            moveHorizontal = Input.GetAxis("Horizontal");
+            moveVertical = Input.GetAxis("Vertical");
+        }
+#else 
+        {
+            controllerInput.Update();
+
+            moveHorizontal = controllerInput.GetAxisLeftThumbstickX();
+            moveVertical = controllerInput.GetAxisLeftThumbstickY();
+        }
+#endif       
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
 
         rb.AddForce(movement * speed);
     }
